60262cf9bbc2aea67425413813a6a7f647b95f06
Dave Kimmel <davidjkimmel@gmail.com>
2024-04-28 09:50:35 -0600

    feat:  Bring in AESO Summary data too

--- AESOGrabber/AESOGrabber.py
+++ AESOGrabber/AESOGrabber.py
@@ -13,19 +13,54 @@ engine = create_engine(DB_URL)
 
 with engine.connect() as conn:
     while True:
+        # Fetch per-asset generation data
         resp = requests.get("https://api.aeso.ca/report/v1/csd/generation/assets/current", headers={"X-API-Key": AESO_API_KEY})
         jsondata = resp.json()
-
         ts = datetime.datetime.strptime(jsondata['return']['last_updated_datetime_utc'], '%Y-%m-%d %H:%M').replace(tzinfo=datetime.timezone.utc)
-        data = []
-        for asset in jsondata['return']['asset_list']:
-            asset['ts'] = ts
-            data.append(asset)        
-
         with conn.begin():
+            data = []
+            for asset in jsondata['return']['asset_list']:
+                asset['ts'] = ts
+                data.append(asset)
             try:
                 conn.execute(text("insert into aeso_generation(ts, asset_id, fuel_type, sub_fuel_type, maximum_capability, net_generation, dispatched_contingency_reserve) "
                                 "values (:ts, :asset, :fuel_type, :sub_fuel_type, :maximum_capability, :net_generation, :dispatched_contingency_reserve)"), data)
+            except sqlalchemy.exc.IntegrityError:
+                # We've already seen this set of data, just continue on
+                pass
+            finally:
+                conn.commit()
+
+        # Fetch summary data
+        resp = requests.get("https://api.aeso.ca/report/v1/csd/summary/current", headers={"X-API-Key": AESO_API_KEY})
+        jsondata = resp.json()
+        ts = datetime.datetime.strptime(jsondata['return']['last_updated_datetime_utc'], '%Y-%m-%d %H:%M').replace(tzinfo=datetime.timezone.utc)
+        with conn.begin():
+            try:
+                data = jsondata['return']
+                data['ts'] = ts
+                # NB: ":dispatched_contigency_reserve_total" is correct as this field is misspelled in the API
+                #      "dispatched_contingency_reserve_total" is the database field with the correct spelling
+                conn.execute(text("insert into aeso_summary(ts, total_max_generation_capability, total_net_generation, net_to_grid_generation, net_actual_interchange, "
+                                  "alberta_internal_load, contingency_reserve_required, dispatched_contingency_reserve_total, dispatched_contingency_reserve_gen, "
+                                  "dispatched_contingency_reserve_other, lssi_armed_dispatch, lssi_offered_volume ) "
+                                  "values (:ts, :total_max_generation_capability, :total_net_generation, :net_to_grid_generation, :net_actual_interchange, "
+                                  ":alberta_internal_load, :contingency_reserve_required, :dispatched_contigency_reserve_total, :dispatched_contingency_reserve_gen, "
+                                  ":dispatched_contingency_reserve_other, :lssi_armed_dispatch, :lssi_offered_volume)"), data)
+                
+                data = []
+                for gendata in jsondata['return']['generation_data_list']:
+                    gendata['ts'] = ts
+                    data.append(gendata)
+                conn.execute(text("insert into aeso_summary_generation(ts, fuel_type, aggregated_maximum_capability, aggregated_net_generation, aggregated_dispatched_contingency_reserve) "
+                                  "values (:ts, :fuel_type, :aggregated_maximum_capability, :aggregated_net_generation, :aggregated_dispatched_contingency_reserve)"), data)
+
+                data = []
+                for interchange in jsondata['return']['interchange_list']:
+                    interchange['ts'] = ts
+                    data.append(interchange)
+                conn.execute(text("insert into aeso_summary_interchange(ts, flow_path, actual_flow) values (:ts, :path, :actual_flow)"), data)
+
             except sqlalchemy.exc.IntegrityError:
                 # We've already seen this set of data, just continue on
                 pass
--- deployment/schema.sql
+++ deployment/schema.sql
@@ -10,3 +10,37 @@ create table aeso_generation(
 	dispatched_contingency_reserve numeric(5),
 	primary key (ts, asset_id)	
 );
+
+
+create table aeso_summary(
+	ts timestamptz,
+	total_max_generation_capability numeric(7),
+	total_net_generation numeric(7),
+	net_to_grid_generation numeric(7),
+	net_actual_interchange numeric(7),
+	alberta_internal_load numeric(7),
+	contingency_reserve_required numeric(7),
+	dispatched_contingency_reserve_total numeric(7),
+	dispatched_contingency_reserve_gen numeric(7),
+	dispatched_contingency_reserve_other numeric(7),
+	lssi_armed_dispatch numeric(7),
+	lssi_offered_volume numeric(7),
+	primary key (ts)
+);
+
+create table aeso_summary_generation(
+	ts timestamptz,
+	fuel_type varchar(30),
+	aggregated_maximum_capability numeric(7),
+	aggregated_net_generation numeric(7),
+	aggregated_dispatched_contingency_reserve numeric(7),
+	primary key (ts, fuel_type)
+);
+
+create table aeso_summary_interchange(
+	ts timestamptz,
+	flow_path varchar(30),
+	actual_flow numeric(7),
+	primary key (ts, flow_path)
+);
+
